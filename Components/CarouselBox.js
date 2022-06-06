0c63eb093973c6fb9fc069a509e00cc83358ba6f
NastyaSmir <101507367+NastyaSmir@users.noreply.github.com>
2022-06-06 19:51:58 +0200

    Add files via upload

---
+++ Components/CarouselBox.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Carousel from "react-bootstrap/Carousel";
+import backgroundImg from '../assets/4.jpg';
+import backgroundImg2 from '../assets/5.jpg';
+
+export default class CarouselBox extends Component {
+    render() {
+        return (
+            <div></div>
+            // <Carousel>
+            //     <Carousel.Item>
+            //         <img
+            //             className="image"
+            //             src={backgroundImg}
+            //         />
+            //         <Carousel.Caption>
+            //             <h3>Автомобильные грузоперевозки по всей стране</h3>
+
+            //         </Carousel.Caption>
+            //     </Carousel.Item>
+            //     <Carousel.Item>
+            //         <img
+            //             className="image"
+            //             src={backgroundImg2}
+            //         />
+            //         <Carousel.Caption>
+            //             <h3>Автомобильные грузоперевозки по всей стране</h3>
+
+            //         </Carousel.Caption>
+            //     </Carousel.Item>
+            // </Carousel>
+        )
+    }
+}   
---
+++ Pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import CarouselBox from "../Components/CarouselBox";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div></div>
+            // <CarouselBox />
+        )
+    }
+}   
